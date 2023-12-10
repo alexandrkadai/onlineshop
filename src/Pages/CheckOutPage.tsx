5a22df628ea26db18f3b70a4b32da0d8bfbaf96c
Alex Kadai <alexandrkadai@gmail.com>
2023-12-10 23:54:47 +0200

    Super

--- src/Pages/CheckOutPage.tsx
+++ src/Pages/CheckOutPage.tsx
@@ -1,14 +1,14 @@
 import ChecOut from "../Components/ChecOut";
 import Header from "../Components/Header";
-import FormComponent from "../Components/FormComponent";
+import FormCheckout from "../Components/FormCheckout";
 type Props = {}
 
 const CheckOutPage = (props: Props) => {
   return (
     <div className="paddingValue">
         <Header/>
-        <FormComponent/>
-        <ChecOut/>
+        <FormCheckout/>
+        
     </div>
   )
 }
