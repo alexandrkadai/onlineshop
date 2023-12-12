6103e57bf3133b69b8309eecdc37984c2f5201a8
Alex Kadai <alexandrkadai@gmail.com>
2023-12-12 14:26:11 +0200

    Novaposhta

--- src/Components/FormCheckout.jsx
+++ src/Components/FormCheckout.jsx
@@ -17,7 +17,12 @@ const FormCheckout = () => {
     const getDepartment = (async) =>
       fetch(url, {
         method: 'POST',
-        body: {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({
           apiKey: 'e12072eca09e540b30ebfe2fa0eed6e2',
           modelName: 'Address',
           calledMethod: 'getWarehouses',
@@ -26,9 +31,8 @@ const FormCheckout = () => {
             Page: '1',
             Limit: '50',
             Language: 'UA',
-            
           },
-        },
+        }),
       })
         .then((response) => response.json())
         .then((data) => {
@@ -39,19 +43,33 @@ const FormCheckout = () => {
   }, [some]);
 
   return (
-    <div>
-      <select id="city"></select>
-      <form>
-        <label htmlFor=""></label>
-        <input type="text" placeholder="sometext" />
+    <div className='mt-[133px]'>
+      
+      <form className="mt-[33px] flex flex-col">
+        <label htmlFor="vity">Enter Your City</label>
+        <input type="text" name="vity" placeholder="City Name"  className='w-[100px]'/>
         <label htmlFor="vity"></label>
         <input name="vity" type="text" placeholder="sometext" />
       </form>
 
-      <div>
-        <span>novaPoshta</span>
+      <div className='mt-[30px]'>
+        <span className='text-[25px]'>Nova Poshta</span>
         <br />
-        <button onClick={clickUseHandler}>Click ME </button>
+        <button className="border-[1px] border-black p-[5px]" onClick={clickUseHandler}>Click ME </button>
+        <div className='mt-[33px]'>
+        <select  className="list-none" >
+        {some ? 
+          cities.data.map((item) => (
+           
+            
+               
+                <option key={item.SiteKey} >{item.Description}</option>
+            
+
+            
+          )) : <span>loading</span>}
+          </select>
+          </div>
       </div>
     </div>
   );
