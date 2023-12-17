8ef7107c984f425e613fa52aa422480c64aa7308
Alex Kadai <alexandrkadai@gmail.com>
2023-12-17 23:42:50 +0200

    NOvaposhta done;4D;4D

--- src/Components/FormCheckout.tsx
+++ src/Components/FormCheckout.tsx
@@ -3,18 +3,27 @@ import classes from './FormChecout.module.scss';
 
 const FormCheckout = () => {
   const [cityChoose, setCitiChoose] = useState<string | null>(null);
+
   const [city, setCity] = useState([]);
-  
+
   const [warhouseChoose, setWarhouseChoose] = useState<string | null>(null);
-  const [warhouseW, setWarhouseW] =  useState([]);
+  const [warhouseW, setWarhouseW] = useState([]);
 
   const [optionsState, setOptionsState] = useState<string>('');
 
+  // input for user city to send for city search 1st step
   const cityOnChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     setTimeout(() => {
-    setCitiChoose(event.target.value);
+      setCitiChoose(event.target.value);
     }, 2000);
+  };
+
+  //Select your city from city list 2nd step
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setOptionsState(event.target.value);
+    console.log(event.target.value);
+    console.log(optionsState);
   };
 
   const warhouseOnChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,40 +31,36 @@ const FormCheckout = () => {
     setWarhouseChoose(event.target.value);
   };
 
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setOptionsState(event.target.value);
-  };
-
-  var url:string = 'https://api.novaposhta.ua/v2.0/json/';
+  var url: string = 'https://api.novaposhta.ua/v2.0/json/';
 
   const key: string = 'e12072eca09e540b30ebfe2fa0eed6e2';
 
-  // Find City od Delivery
+  // Find City of Delivery
   useEffect(() => {
-    if(cityChoose){
-    const getDepartment = async (): Promise<any> =>
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-
-        body: JSON.stringify({
-          apiKey: key,
-          modelName: 'Address',
-          calledMethod: 'getCities',
-          methodProperties: {
-            FindByString: cityChoose,
+    if (cityChoose) {
+      const getDepartment = async (): Promise<any> =>
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
           },
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setCity(data.data);
-        });
-    getDepartment();
-      }
+
+          body: JSON.stringify({
+            apiKey: key,
+            modelName: 'Address',
+            calledMethod: 'getCities',
+            methodProperties: {
+              FindByString: cityChoose,
+            },
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setCity(data.data);
+          });
+      getDepartment();
+    }
   }, [cityChoose]);
 
   // Choose an delivery warehouse
@@ -85,9 +90,7 @@ const FormCheckout = () => {
           setWarhouseW(data.data);
         });
     getDepartment();
-
-    
-  }, [warhouseChoose]);
+  }, [optionsState]);
 
   return (
     <div className={classes.fromWraper}>
@@ -107,24 +110,31 @@ const FormCheckout = () => {
         <h4 className={classes.deliveryTitle}>Delivery Method</h4> <br />
         <span className="text-[20px]">Nova Poshta</span>
         <br />
-        
         <div className={classes.warhouseWrap}>
-        <label htmlFor="city">Enter Your City</label>
-        <input type="text" name="city" placeholder="City Name" onChange={cityOnChangeHandler} />
+          <label htmlFor="city">Enter Your City</label>
+          <input type="text" name="city" placeholder="City Name" onChange={cityOnChangeHandler} />
 
           <select className={classes.warhouses} value={optionsState} onChange={handleSelectChange}>
-            {city ? city.map((item: any) => (
-              <option key={item.Description} value={item.Description}>{item.Description}</option>
-            )) : <option> loading </option>}
+            {city ? (
+              city.map((item: any) => (
+                <option key={item.Description} value={item.Description}>
+                  {item.Description}
+                </option>
+              ))
+            ) : (
+              <option> loading </option>
+            )}
           </select>
-
-          </div>
-
+        </div>
         <div className={classes.warhouseWrap}>
-          <select className={classes.warhouses} >
+          <select className={classes.warhouses}>
             <option>Виберіть Відділення</option>
             {warhouseW ? (
-              warhouseW.map((item: any) => <option key={item.SiteKey} value={item.Description}>{item.Description}</option>)
+              warhouseW.map((item: any) => (
+                <option key={item.SiteKey} value={item.Description}>
+                  {item.Description}
+                </option>
+              ))
             ) : (
               <option>loading</option>
             )}
