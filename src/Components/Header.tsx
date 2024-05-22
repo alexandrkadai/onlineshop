b75b3479697f7045a9d757a6efe4d6e3fd55dc48
Alex Kadai <alexandrkadai@gmail.com>
2024-05-22 15:14:38 +0300

    Corners and gif

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom';
 import Cart from './Cart';
 import logo from '../assets/logo.webp';
 import classes from './Header.module.css';
+import corner from '../assets/corner.svg';
+import contacts from '../assets/contacts.png';
 
 type Props = {
   toggleCart: () => void;
 };
 
 const Header = ({ toggleCart }: Props) => {
-  
   return (
     <>
       <nav className={classes.mainNav}>
@@ -32,7 +33,11 @@ const Header = ({ toggleCart }: Props) => {
           <img className={classes.logoImg} src={logo} alt="logo" />
         </Link>
         <Cart toggleCart={toggleCart} />
+       
       </div>
+      
+        <img  className={classes.cornerPointer} src={contacts} alt="contacts" />
+      <img  className={classes.corner} src={corner} alt="paper corner" />
     </>
   );
 };
--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,6 @@ import sweet3 from './sweet1/sweet3.jpeg';
 import sweet4 from './sweet1/sweet4.avif';
 import logo from './logo.webp';
 import Sleep from './RowdYSleep.gif';
-
-export { sweet1, sweet2, sweet3, sweet4, logo, Sleep };
-
+import corner from './corner.svg';
+import contacts from './contacts.png';
+export { sweet1, sweet2, sweet3, sweet4, logo, Sleep, corner, contacts };
