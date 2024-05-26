9696544c645ac6a7b404b4011c246dedd723c7c5
Alex Kadai <alexandrkadai@gmail.com>
2024-05-26 03:22:30 +0300

    Animate new method

--- src/Components/ProductComponents/ProductCard.tsx
+++ src/Components/ProductComponents/ProductCard.tsx
@@ -2,50 +2,48 @@ import sweet1 from '../../assets/sweet1/sweet1.avif';
 import { Link } from 'react-router-dom';
 import classes from './ProductCard.module.scss';
 
-
 const ProductCard = () => {
   return (
     <div className={classes.mainWrap}>
+      <div className={classes.productWrap}>
+        <Link to="/super">
+          <img className={classes.productImg} src={sweet1} alt="product" loading="lazy" />
+        </Link>
+        <div className={classes.producttextWrap}>
+          <span className={classes.productName}>top shirt</span>
+          <span className={classes.productPrice}>3099 &#8372;</span>
+        </div>
+      </div>
 
-    <div className={classes.productWrap}>
-      <Link to="/super">
-        <img className={classes.productImg} src={sweet1} alt="product" loading="lazy"/>
-      </Link>
-      <div className={classes.producttextWrap}>
-      <span className={classes.productName}>top shirt</span>
-      <span className={classes.productPrice}>3099 &#8372;</span>
+      {/* Never Will Be  */}
+      <div className={classes.productWrap}>
+        <Link to="/super">
+          <img className={classes.productImg} src={sweet1} alt="product" loading="lazy" />
+        </Link>
+        <div className={classes.producttextWrap}>
+          <span className={classes.productName}>top shirt</span>
+          <span className={classes.productPrice}>3099 &#8372;</span>
+        </div>
       </div>
-    </div>
-    
-    {/* Never Will Be  */}
-    <div className={classes.productWrap}>
-      <Link to="/super">
-        <img className={classes.productImg} src={sweet1} alt="product"  loading="lazy"/>
-      </Link>
-      <div className={classes.producttextWrap}>
-      <span className={classes.productName}>top shirt</span>
-      <span className={classes.productPrice}>3099 &#8372;</span>
+      <div className={classes.productWrap}>
+        <Link to="/super">
+          <img className={classes.productImg} src={sweet1} alt="product" loading="lazy" />
+        </Link>
+        <div className={classes.producttextWrap}>
+          <span className={classes.productName}>top shirt</span>
+          <span className={classes.productPrice}>3099 &#8372;</span>
+        </div>
       </div>
-    </div>
-    <div className={classes.productWrap}>
-      <Link to="/super">
-        <img className={classes.productImg} src={sweet1} alt="product"  loading="lazy"/>
-      </Link>
-      <div className={classes.producttextWrap}>
-      <span className={classes.productName}>top shirt</span>
-      <span className={classes.productPrice}>3099 &#8372;</span>
+      {/* Never Will Be  */}
+      <div className={classes.productWrap}>
+        <Link to="/super">
+          <img className={classes.productImg} src={sweet1} alt="product" loading="lazy" />
+        </Link>
+        <div className={classes.producttextWrap}>
+          <span className={classes.productName}>top shirt</span>
+          <span className={classes.productPrice}>3099 &#8372;</span>
+        </div>
       </div>
-    </div>
-    {/* Never Will Be  */}
-    <div className={classes.productWrap}>
-      <Link to="/super">
-        <img className={classes.productImg} src={sweet1} alt="product" loading="lazy" />
-      </Link>
-      <div className={classes.producttextWrap}>
-      <span className={classes.productName}>top shirt</span>
-      <span className={classes.productPrice}>3099 &#8372;</span>
-      </div>
-    </div>
     </div>
   );
 };
--- src/Components/Ui/AnimationComp.tsx
+++ src/Components/Ui/AnimationComp.tsx
@@ -5,34 +5,49 @@ import Woke from '../../assets//RowdYWoke.gif';
 
 const AnimationComp = () => {
   const dispatch = useDispatch();
-  
-  document.body.style.overflow = "hidden";
+
+  document.body.style.overflow = 'hidden';
+
   const clickImageHadler = () => {
-    let img: HTMLImageElement = document.querySelector('#indigo');
+    let sleep: HTMLImageElement = document.querySelector('#sleepImg');
     let wrp: HTMLImageElement = document.querySelector('#aniWrap');
-
-    img.src = Woke;
-
+    let woke: HTMLImageElement = document.querySelector('#wokeImg');
+    sleep.style.cssText = 'display: none';
+    woke.style.cssText = 'display: block';
     setTimeout(() => {
-      wrp.style.cssText = 'opacity:0; transition: 1s';
       setTimeout(() => {
         wrp.style.display = 'none';
         dispatch(setAnimate());
-        document.body.style.overflow = "visible";
+        document.body.style.overflow = 'visible';
       }, 500);
     }, 6000);
   };
-  setTimeout(() => {
-    dispatch(setAnimate());
-    
-  }, 7000);
+
+  // setTimeout(() => {
+  //   dispatch(setAnimate());
+  // }, 7000);
 
   return (
     <>
       <div className="w-[100vw] h-[100vh] cursor-pointer" id="aniWrap" onClick={clickImageHadler}>
         <div className="absolute top-28 left-0 right-0 m-auto text-center">
-          <img className="absolute top-10 left-0 right-0 m-auto text-center" src={Sleep} id="indigo" loading="eager"/>
-          <span className="absolute top-96 left-0 right-0 m-auto text-center cursor-pointer" >Start</span>
+          <img
+            className="absolute top-10 left-0 right-0 m-auto text-center w-96 block"
+            src={Sleep}
+            id="sleepImg"
+            loading="eager"
+            alt="git animation rowsy sleep"
+          />
+          <img
+            className="none absolute top-10 left-0 right-0 m-auto text-center w-96 hidden"
+            src={Woke}
+            id="wokeImg"
+            loading="eager"
+            alt="git animation rowsy woke"
+          />
+          <span className="absolute top-96 left-0 right-0 m-auto text-center cursor-pointer">
+            Start
+          </span>
         </div>
       </div>
     </>
